0a304f8afc944f50b06cceae94f0716172550bc5
Soy-Boy <scuwangjun@hotmail.com>
2015-07-05 23:27:59 +0800

    flashlight
    王骏的手电筒

---
+++ src/com/scuwnagjun/flashlight/MainActivity.java
@@ -0,0 +1,68 @@
+package com.scuwnagjun.flashlight;
+
+import com.scuwangjun.flashlight.R;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+	private Button button;
+	private LinearLayout main;
+	private Camera camera;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		button = (Button) findViewById(R.id.main_button);
+		main = (LinearLayout) findViewById(R.id.main_id);
+
+		button.setTag("2");
+		
+		button.setText("关闭");
+		button.setTextColor(getResources().getColor(R.color.black));
+		main.setBackgroundColor(getResources().getColor(
+				R.color.white));
+
+		camera = Camera.open();
+		Parameters params = camera.getParameters();
+		params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		camera.setParameters(params);
+		camera.startPreview(); // 开始亮灯
+
+		button.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				if (button.getTag().equals("1")) {
+					button.setText("关闭");
+					button.setTextColor(getResources().getColor(R.color.black));
+					main.setBackgroundColor(getResources().getColor(
+							R.color.white));
+
+					camera = Camera.open();
+					Parameters params = camera.getParameters();
+					params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+					camera.setParameters(params);
+					camera.startPreview(); // 开始亮灯
+
+					button.setTag("2");
+				} else if (button.getTag().equals("2")) {
+					button.setText("打开");
+					button.setTextColor(getResources().getColor(R.color.white));
+					main.setBackgroundColor(getResources().getColor(
+							R.color.black));
+
+					camera.stopPreview(); // 关掉亮灯
+					camera.release(); // 关掉照相机
+
+					button.setTag("1");
+				}
+			}
+		});
+	}
+}
