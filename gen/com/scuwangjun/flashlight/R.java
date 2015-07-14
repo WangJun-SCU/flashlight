8373faf43e646fd50a34d12372d2bbf1b9a2f90d
Soy-Boy <scuwangjun@hotmail.com>
2015-07-14 11:21:57 +0800

    手电-7/14
    增加退出功能

--- gen/com/scuwangjun/flashlight/R.java
+++ gen/com/scuwangjun/flashlight/R.java
@@ -17,9 +17,11 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_launcher2=0x7f020001;
+        public static final int ic_launcher3=0x7f020002;
     }
     public static final class id {
         public static final int main_button=0x7f070001;
+        public static final int main_exit=0x7f070002;
         public static final int main_id=0x7f070000;
     }
     public static final class layout {
--- src/com/scuwnagjun/flashlight/MainActivity.java
+++ src/com/scuwnagjun/flashlight/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 	private Button button;
+	private Button exitId;
 	private LinearLayout main;
 	private Camera camera;
 
@@ -20,7 +21,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		button = (Button) findViewById(R.id.main_button);
+		exitId = (Button) findViewById(R.id.main_exit);
 		main = (LinearLayout) findViewById(R.id.main_id);
+		
+		exitId.setVisibility(View.INVISIBLE);
 
 		button.setTag("2");
 		
@@ -49,6 +53,8 @@ public class MainActivity extends Activity {
 					params.setFlashMode(Parameters.FLASH_MODE_TORCH);
 					camera.setParameters(params);
 					camera.startPreview(); // 开始亮灯
+					
+					exitId.setVisibility(View.INVISIBLE);
 
 					button.setTag("2");
 				} else if (button.getTag().equals("2")) {
@@ -59,9 +65,18 @@ public class MainActivity extends Activity {
 
 					camera.stopPreview(); // 关掉亮灯
 					camera.release(); // 关掉照相机
+					
+					exitId.setVisibility(View.VISIBLE);
 
 					button.setTag("1");
 				}
+			}
+		});
+		
+		exitId.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				System.exit(0);
 			}
 		});
 	}
